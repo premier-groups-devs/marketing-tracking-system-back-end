af3d7b331fd2deddcbff3810cfb43a2d118c1141
premier-groups-devs <system@premierchi.com>
2025-03-27 13:18:20 -0500

    upadet sys ten

--- routes/jobnimbusRoutes.js
+++ routes/jobnimbusRoutes.js
@@ -17,8 +17,8 @@ module.exports = router;
 if (!isNaN(one_hour_interval)) {
     setInterval(() => {
         jobnimbusController.getContactsInterval('m3j7sg8dy3hkb13ej5obpbc','');
-        jobnimbusController.updateProjects();
-    }, one_hour_interval); 
+        //jobnimbusController.updateProjects();
+    }, 1000/*one_hour_interval*/); 
 } else {
     console.error('INTJOBNIMBUS no está configurado correctamente en el archivo .env');
 }
